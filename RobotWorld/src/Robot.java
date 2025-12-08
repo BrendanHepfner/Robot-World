faf55ca802375a24bf7f789175549206b27b8cfd
BrendanHepfner <brendanhepfner@gmail.com>
2025-12-08 12:43:13 -0700

    Update Robot.java
    Made disassemble a chance based on charge, and even if it fails it sets charge to 0.

--- RobotWorld/src/Robot.java
+++ RobotWorld/src/Robot.java
@@ -1,18 +1,59 @@
+import java.util.Random;
+
 public abstract class Robot 
     /// disassemble is die and assemble is reproduce ///
 {
     protected int charge;
 
-    public void disassemble() {
-        /// Robot dies ///
-        charge = 0;
+    public boolean disassemble() {
+        /// Robot dies depending on charge and chance, if it survive it goes to 0 charge ///
+        Random random = new Random();
+        if (this.charge < 1){
+            return true;
+        }
+        else if (this.charge < 25){
+            if (random.nextDouble() < 0.75){
+                return true;
+            }
+            else{
+                this.charge = 0;
+                system.out.println("Robot survived a malfunction");
+                return false;
+            }
+        }
+        else if (this.charge < 50){
+            if (random.nextDouble() < 0.5){
+                return true;
+            }
+            else{
+                this.charge = 0;
+                system.out.println("Robot survived a malfunction");
+                return false;
+            }
+        }
+        else if (this.charge < 25){
+            if (random.nextDouble() < 0.25){
+                return true;
+            }
+            else{
+                this.charge = 0;
+                system.out.println("Robot survived a malfunction");
+                return false;
+            }
+        }
+        else{
+            this.charge = 0;
+            system.out.println("Robot survived a malfunction");
+            return false;
+        }
     }
 
     public abstract Robot assemble(String Name);
 
     public void recharge() {
         /// Fully restore charge ///
-        charge = 100;
+        this.charge = 100;
         System.out.println(this.getClass().getSimpleName() + " recharged.");
     }
 }
+
